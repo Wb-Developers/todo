b77f8bae055d63208185c606d86d6d1469164844
Wb-Developers <143939170+Wb-Developers@users.noreply.github.com>
2025-05-26 23:16:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 useEffect(()=>{
   if(!localStorage){return}
   let previousList = JSON.parse(localStorage.getItem('list'));
-  setTasks(previousList);
+  previousList && setTasks(previousList);
 },[])
 
 function addTodo() {
